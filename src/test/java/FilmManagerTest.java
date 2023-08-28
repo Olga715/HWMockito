23fc6d60de97a6ef6a43bf795a950816e07f2a3e
Olga <olyab.715@gmail.com>
2023-08-28 21:18:05 +0500

    Change Mockito

---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void emptyManager() {
+        FilmManager manager = new FilmManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findFilm() {
+        FilmManager manager = new FilmManager();
+
+        manager.add("Film I");
+
+        String[] expected = {"Film I"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findMoreFilms() {
+        FilmManager manager = new FilmManager();
+
+        manager.add("Film I");
+        manager.add("Film II");
+        manager.add("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastFilms() {
+        FilmManager manager = new FilmManager();
+
+        manager.add("Film I");
+        manager.add("Film II");
+        manager.add("Film III");
+
+        String[] expected = {"Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findMoreLastFilms() {
+        FilmManager manager = new FilmManager();
+
+        manager.add("Film I");
+        manager.add("Film II");
+        manager.add("Film III");
+        manager.add("Film IV");
+        manager.add("Film V");
+
+        String[] expected = {"Film V", "Film IV", "Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
